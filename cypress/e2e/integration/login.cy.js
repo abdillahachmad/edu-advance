e6de0065a07f04f6a3d93e549a0517c7378eff2f
Hendrich <hendri.christianto24@gmail.com>
2024-10-17 20:21:12 +0700

    fix file login.cy.js

--- cypress/e2e/integration/login.cy.js
+++ cypress/e2e/integration/login.cy.js
@@ -2,9 +2,10 @@
 
 describe('Login/Logout test', () => { 
     beforeEach(() => {
-        cy.visit('http://zero.webappsecurity.com/index.html')
-        cy.url().should('include', 'index.html')
-        cy.get('#signin_button').click()
+        
+            cy.visit('http://zero.webappsecurity.com/index.html')
+            cy.url().should('include', 'index.html')
+            cy.get('#signin_button').click()
     });
 
     it('Should try to login with invalid data', () => {
@@ -15,8 +16,11 @@ describe('Login/Logout test', () => {
     });
 
     it('Should display error message', () => {
-        cy.get('.alert-error').should('be.visible').and('contain.text', ' Login and/or password are wrong.')
+        cy.get('input[name="submit"]').click()
+        cy.wait(2000) // Menunggu 2 detik sebelum memeriksa elemen
+        cy.get('.alert.alert-error').should('be.visible').and('contain.text', 'Login and/or password are wrong.')
     });
+    
 
     it('Should login to application with valid data', () => {
         cy.fixture("user").then(user => {
@@ -34,20 +38,32 @@ describe('Login/Logout test', () => {
     });
 
     it('Should logout from the application', () => {
-        cy.contains('username').click()
+        cy.fixture("user").then(user => {
+            const username = user.username
+            const password = user.password
+
+            cy.get('#user_login').clear()
+            cy.get('#user_login').type(username)
+            cy.get('#user_password').clear()
+            cy.get('#user_password').type(password)
+            cy.get('input[name="submit"]').click()
+
+            cy.get('h2').should('contain.text', 'Cash Accounts')
+        })
+       
+        // Klik pada elemen dropdown-toggle (ikon pengguna di bagian kanan atas)
+        cy.get('.dropdown-toggle').eq(1).click()  // Klik dropdown pertama
+
+        // Klik link logout dari dropdown
         cy.get('#logout_link').click()
-        //TUGAS TEST CASE LOGUT BELUM ADA ASSERTION
+
         // Assertion: Memastikan URL berubah ke halaman login atau homepage
         cy.url().should('include', 'index.html')
-         // Assertion: Memastikan tombol "Sign in" kembali terlihat, yang artinya pengguna sudah logout
+
+        // Assertion: Memastikan tombol "Sign in" kembali terlihat, yang artinya pengguna sudah logout
         cy.get('#signin_button').should('be.visible')
-         // Assertion tambahan: Memastikan elemen yang hanya ada saat login sudah tidak terlihat
+
+        // Assertion tambahan: Memastikan elemen yang hanya ada saat login sudah tidak terlihat
         cy.get('#logout_link').should('not.exist')
-
-
-
-
-
-        //cy.get('strong').should('contain.text', 'Home')
     });
- })
+})
